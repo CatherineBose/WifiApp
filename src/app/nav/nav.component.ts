b5a83381151e318ed197765d11e57c83af24ab25
Ryan Kistner <RyanGKistner@Patricks-MacBook-Pro-2.local>
2017-12-22 13:20:07 -0800

    updated things

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Route } from '@angular/router/src/config';
+import { Subject } from 'rxjs/Subject';
+import { MapService } from '../services/map.service';
+import 'rxjs/add/operator/switchMap';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,14 +10,18 @@ import { Route } from '@angular/router/src/config';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  search;
-  constructor(private _route : Router) { }
+  term$ = new Subject<string>();
+  locations;
+
+  constructor(
+    private _mapService: MapService,
+    private _searchService: SearchService
+  ) { }
 
   ngOnInit() {
+    this._searchService
+      .search(this.term$)
+      .subscribe(locations => this.locations = locations)
   }
-  onSubmit(){
-    console.log(this.search);
-    this._route.navigate([this.search]);;
-    // Seet active to true
-  }
+
 }
